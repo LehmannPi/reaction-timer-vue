7cf5aa2c59999502a0fbd9ac7f328c158f6dafe2
Filipe <filipelehmannp@gmail.com>
2021-06-23 10:23:04 -0300

    Finishing Touches

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,52 @@
+
+// * Create boolean for game status (on/off) [x]
+// * Create timer - counting randomly between 2 and 7 sec [x]
+// * Show the block that will be shown in the screen [x]
+// * Tie the block appearance to the status isPlaying [x]
+// * Disable button while game is currently going on [x]
+// * Receive emission of Block component end handle it's variable in a function [x]
+// * 
+
 <template>
   <h1>Reaction Timer</h1>
+  <p>Click on the big button as soon as it appears</p>
+  <p class="br">(Clique no botão grande logo que ele aparecer)</p>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" v-bind:delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score" />
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from "./components/Block.vue";
+import Results from "./components/Results.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Block, Results },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null, // will be set after
+      score: null,
+      showResults: false,
+      // showScore: false,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.showScore = false;
+      // console.log(this.delay);
+    },
+    endGame(reacTime) {
+      this.score = reacTime;
+      this.isPlaying = false;
+      this.showResults = true;
+    },
+  },
+};
 </script>
 
 <style>
@@ -21,5 +57,25 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.br {
+  font-size: 0.9em;
+  font-style: italic;
+  margin: 16px;
+}
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,60 @@
+// * Create status to wait for random delay [x]
+// * Use setTimeout function with the delay prop to show block [x]
+// * Start timer as block appears []
+  // ! Create functions (start/stop) and correspondent variables
+
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">Click Me!</div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0,
+    };
+  },
+  mounted() {
+    // console.log("component mount");
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+    }, this.delay);
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      // console.log(this.reactionTime);
+      this.$emit("end", this.reactionTime);
+    },
+  },
+  //   updated() {
+  //     console.log("component updated");
+  //   },
+  //   unmounted() {
+  //       console.log('unmounted')
+  //   }
+};
+</script>
+
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background: #42b883;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+
+/* Vue colors: #42b883 | #35495e */
+</style>
